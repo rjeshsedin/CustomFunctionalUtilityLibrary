a4f8b36836f6ce5e8cbbc5b15d55f21583d045d6
Rajesh <rajesh@example.com>
2026-04-10 12:13:22 +0530

    removed extra files

--- customUtils.ts
+++
@@ -1,6 +0,0 @@
-export function pluck(path: string| string[], array: any[]){
-    const getValue=(obj:any, path:string)=>{
-        return path.split('.').reduce((acc,key)=>acc?.[key],obj);
-    };
-    return array.map(item=>)
-}
